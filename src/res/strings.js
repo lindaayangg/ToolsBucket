e4ff09ba7e2c754240a28e4d4d652011578e906c
lindaayangg <lindayang0928@hotmail.com>
2019-12-14 21:06:16 -0600

    Refactor sidebar

---
+++ src/res/strings.js
@@ -0,0 +1,26 @@
+export const strings = {
+  menu: {
+    strings: {
+      header: 'String Tools',
+      spaceRemover: 'Space Remover',
+      extraSpaceRemover: 'Extra Space Remover',
+      dashRemover: 'Dash Remover',
+      underscoreRemover: 'Underscore Remover',
+      duplicateLineRemover: 'Duplicate Line Remover',
+      emptyLineRemover: 'Empty Line Remover',
+      lineBreakRemover: 'Line Break Remover',
+      camelCaseGenerator: 'Camel Case Generator',
+      snakeCaseGenerator: 'Snake Case Generator',
+      kababCaseGenerator: 'Kabab Case Generator',
+      snakeCase2CamelCase: 'Snake Case to Camel Case Converter',
+      camelCase2SnakeCase: 'Camel Case to Snake Case Converter',
+      stringReverser: 'String Reverser',
+      wordReverser: 'Word Reverser',
+      wordSorter: 'Word Sorter',
+      textLineReverser:' Text Line Reverser',
+      textLineSorter: 'Text Line Sorter',
+      image2Base64Converter: 'Image to Base64 Converter',
+      base642ImageConverter: 'Base64 to Image Converter'
+    }
+  }
+};
